13205db212a3dc6d46e7443fdbdded69ce6c7fd0
Avni Sharma <16avnisharma@gmail.com>
2019-08-08 18:57:28 +0530

    drawstyle example

---
+++ example_graph.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(1,14,0.1)
+y = np.sin(x/2)
+
+
+fig, ax = plt.subplots(figsize=(15,5))
+_ = ax.plot(x, y, drawstyle='steps-post')
+_ = ax.set_aspect('equal')
+plt.show()
